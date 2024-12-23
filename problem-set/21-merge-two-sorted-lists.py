62be1d4d027eb258cfefb33f0152a3b49c12837a
TunTauk <tauktun628@gmail.com>
2024-12-23 19:21:28 +0700

    fix merge sort

--- problem-set/21-merge-two-sorted-lists.py
+++ problem-set/21-merge-two-sorted-lists.py
@@ -1,46 +1,29 @@
-# Definition for singly-linked list.
+
+from typing import Optional
+
 class ListNode(object):
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-        
-
 
 class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        if not list1:
-            return list2
-        if not list2:
-            return list1
-        
-        is_list1_small = list1.val <= list2.val
-        answer = ListNode(list1.val) if is_list1_small else ListNode(list2.val)
-        temp = answer
-        
-        if is_list1_small:
-            list1 = list1.next
-        else:
-            list2 = list2.next
-            
-        while list1 or list2:
-            if not list1:
-                temp.next = ListNode(list2.val)
-                list2 = list2.next
-                temp = temp.next
-                continue
-            
-            if not list2:
-                temp.next = ListNode(list1.val)
-                list1 = list1.next
-                temp = temp.next
-                continue
-            
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        result = ListNode()
+        curr = result
+        while list1 and list2:
             if list1.val <= list2.val:
-                temp.next = ListNode(list1.val)
+                curr.next = ListNode(list1.val)
                 list1 = list1.next
-                temp = temp.next
-                continue
-            temp.next = ListNode(list2.val)
+            else:
+                curr.next = ListNode(list2.val)
+                list2 = list2.next
+            curr = curr.next
+        while list1:
+            curr.next = ListNode(list1.val)
+            list1 = list1.next
+            curr = curr.next
+        while list2:
+            curr.next = ListNode(list2.val)
             list2 = list2.next
-            temp = temp.next
-        return answer
+            curr = curr.next
+        return result.next
--- problem-set/88-merge-sorted-array.py
+++ problem-set/88-merge-sorted-array.py
@@ -4,7 +4,22 @@ from typing import List
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-      pass
+      i = m + n - 1
+      j = m - 1
+      k = n - 1
+      while j >= 0 and k >= 0:
+        if nums1[j] >= nums2[k]:
+          nums1[i] = nums1[j]
+          j -= 1
+        else:
+          nums1[i] = nums2[k]
+          k -= 1
+        i -= 1
+      while k >= 0:
+        nums1[i] = nums2[k]
+        k -= 1
+        i -= 1
+        
 
 s = Solution()
 print("solution ",s.merge([1,2,3,0,0,0], 3, [2,5,6], 3))
